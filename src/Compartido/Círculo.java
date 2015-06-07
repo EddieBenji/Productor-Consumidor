b31cda92f4ae05d48244819a8e8abf2007f8733c
Lalo_Canche <eduardo_benji@hotmail.com>
2015-06-07 13:05:31 -0500

    Quitanto atributos innecesarios

--- src/Compartido/Círculo.java
+++ src/Compartido/Círculo.java
@@ -8,69 +8,16 @@ package Compartido;
 public class Círculo {
 
 
-    private String name;
-    private boolean available;
-    private final float PI = (float) 3.1416;
+    private final float PI;
     public float radio;
     public float radioSQR;
     public float area;
 
-
-    public Círculo() {
-        this.name = "Transacciones de una Circunferencia";
-        this.available = false;
-        this.radio = (float) 0.0;
-    }
-
     public Círculo(float radio) {
-        this.name = "Transacciones de una Circunferencia";
-        this.available = false;
         this.radio = radio;
+        PI = (float) 3.1416;
     }
 
-    public synchronized void setName(String name) throws InterruptedException {
-        while (available) {
-            wait();
-        }
-        this.name = name;
-        available = true;
-
-        notify();
-    }
-
-    public synchronized void setRadio(float radio) throws InterruptedException {
-        while (available) {
-            wait();
-        }
-        this.radio = radio;
-        available = true;
-        notify();
-    }
-
-    public synchronized float getRadio() throws InterruptedException {
-        while (!available) {
-            wait();
-        }
-        available = false;
-
-        return radio;
-    }
-
-    public synchronized String getName() throws InterruptedException {
-        while (!available) {
-            Thread.sleep(5000);
-            wait();
-        }
-        available = false;
-        notify();
-
-        return name;
-    }
-
-    public synchronized void getArea() throws InterruptedException {
-        this.area = PI * radioSQR;
-        notify();
-    }
 
     public synchronized void calculateRadioRadio() throws InterruptedException {
         System.out.println("Esperando 5 segundos");
@@ -85,15 +32,5 @@ public class Círculo {
         this.area = PI * radioSQR;
         notify();
 
-    }
-
-
-    public float getPI() throws InterruptedException {
-        while (!available) {
-            wait();
-        }
-        available = false;
-
-        return PI;
     }
 }
